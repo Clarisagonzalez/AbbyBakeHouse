958d876ad44e761d75bed27965a73ebecc48906a
Clarisagonzalez <136874433+Clarisagonzalez@users.noreply.github.com>
2024-07-19 13:55:29 -0400

    added images + color theme + js logic + tailwind

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-// JavaScript for smooth scrolling
+// Smooth scrolling for nav bar links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -8,4 +8,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+  // JavaScript to toggle the mobile menu
+document.getElementById('nav-toggle').addEventListener('click', function () {
+    const menu = document.getElementById('nav-menu');
+    menu.classList.toggle('hidden');
+  });
   
+  // Basic form validation
+  function validateForm() {
+    var name = document.forms["orderForm"]["name"].value;
+    var email = document.forms["orderForm"]["email"].value;
+    var orderDetails = document.forms["orderForm"]["orderDetails"].value;
+    if (name == "" || email == "" || orderDetails == "") {
+      alert("All fields must be filled out");
+      return false;
+    }
+  }
